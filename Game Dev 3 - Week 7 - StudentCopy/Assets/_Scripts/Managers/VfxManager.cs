688a1a6fe241f1b6d9cc800d0dc61b0771a5017b
Brooks <O.Brooks5757@student.leedsbeckett.ac.uk>
2024-11-07 10:17:30 +0000

    VfxManager script implemented

---
+++ Game Dev 3 - Week 7 - StudentCopy/Assets/_Scripts/Managers/VfxManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameDevWithMarco.Singleton;
+
+namespace GameDevWithMarco.Managers
+{
+    public class VfxManager : Singleton<VfxManager>
+    {
+        public void HitStop(float stopDuration)
+        {
+            StartCoroutine(HitStopCoroutine(stopDuration));
+        }
+
+        IEnumerator HitStopCoroutine(float duration)
+        {
+            Time.timeScale = 0;
+            yield return new WaitForSeconds(duration);
+            Time.timeScale = 1;
+        }
+    }
+}
